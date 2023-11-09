430b3d85b27b6536b215f94d41e66723b16b45ca
Junming WANG <51500826+jmwang0117@users.noreply.github.com>
2023-11-10 06:47:53 +0800

    Create create_container.sh

---
+++ Docker/create_container.sh
@@ -0,0 +1,14 @@
+xhost local:root
+docker create -t --name ros_gui --runtime nvidia  -e NVIDIA_VISIBLE_DEVICES=all \
+    --gpus all \
+    -e DISPLAY=$DISPLAY \
+    -e CYCLONEDDS_URI=$CYCLONEDDS_URI \
+    -e XAUTHORITY=$XAUTHORITY \
+    --network host \
+    --ipc host \
+    --privileged \
+    --shm-size 8G \
+    -v /dev:/dev \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $XAUTHORITY:$XAUTHORITY \
+    ros_gui bash
