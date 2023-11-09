5cbca358c2a38e4d03d7facf303c8c62672c089b
Junming WANG <51500826+jmwang0117@users.noreply.github.com>
2023-11-10 07:54:58 +0800

    Update create_container.sh

--- Docker/create_container.sh
+++ Docker/create_container.sh
@@ -1,13 +1,16 @@
 xhost local:root
-docker create -t --name ros_gui \
+docker create -t --name ros_gui --runtime nvidia  -e NVIDIA_VISIBLE_DEVICES=all \
+    --gpus all \
     -e DISPLAY=$DISPLAY \
     -e CYCLONEDDS_URI=$CYCLONEDDS_URI \
     -e XAUTHORITY=$XAUTHORITY \
     --network host \
+    --restart=on-failure \
+    --cap-add=SYS_PTRACE \
     --ipc host \
     --privileged \
     --shm-size 8G \
     -v /dev:/dev \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $XAUTHORITY:$XAUTHORITY \
-    ros_gui bash
+    2d059149e4d8 bash
